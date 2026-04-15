74db631d1e1df06eebd17697894b024a9db7e081
plskyuwu <gottsteinmatej@gmail.com>
2026-04-15 21:27:16 +0200

    feat(viewmodel): add button handling logic and inject database dependency

--- hydration-and-calorie-tracker/ViewModels/MainWindowViewModel.cs
+++ hydration-and-calorie-tracker/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace hydration_and_calorie_tracker.ViewModels;
+﻿using System;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using hydration_and_calorie_tracker.Models;
+
+namespace hydration_and_calorie_tracker.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public ICommand ClickCommand { get; }
+
+    private readonly AppDatabase _db;
+
+    public MainWindowViewModel(AppDatabase db)
+    {
+        _db = db;
+
+        ClickCommand = new RelayCommand(OnClick);
+    }
+
+    private void OnClick()
+    {
+        Console.WriteLine("Button clicked");
+    }
 }
