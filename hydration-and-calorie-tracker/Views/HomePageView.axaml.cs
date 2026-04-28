46b9a7c012df1c39711c8276da3c1fbe20540567
plskyuwu <gottsteinmatej@gmail.com>
2026-04-28 22:55:38 +0200

    feat(ui): add views for all pages

---
+++ hydration-and-calorie-tracker/Views/HomePageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace hydration_and_calorie_tracker.Views;
+
+public partial class HomePageView : UserControl
+{
+    public HomePageView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ hydration-and-calorie-tracker/Views/ItemsPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace hydration_and_calorie_tracker.Views;
+
+public partial class ItemsPageView : UserControl
+{
+    public ItemsPageView()
+    {
+        InitializeComponent();
+    }
+}
