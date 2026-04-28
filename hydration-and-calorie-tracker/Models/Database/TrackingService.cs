b858aa70c31d0c9bd11f94d83a13b3f18a4a7da5
plskyuwu <gottsteinmatej@gmail.com>
2026-04-29 00:21:56 +0200

    feat(models): add hydration and calorie goal properties to TrackingService

--- hydration-and-calorie-tracker/Models/Database/TrackingService.cs
+++ hydration-and-calorie-tracker/Models/Database/TrackingService.cs
@@ -1,13 +1,36 @@
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace hydration_and_calorie_tracker.Models.Database;
 
 public class TrackingService(
     IRepository<Item, int> items,
-    IRepository<Entry, int> entries)
+    IRepository<Entry, int> entries,
+    IRepository<Setting, string> settings)
 {
     private readonly IRepository<Item, int> _items = items;
     private readonly IRepository<Entry, int> _entries = entries;
+    private readonly IRepository<Setting, string> _settings = settings;
+
+    public decimal HydrationGoal
+    {
+        get => decimal.Parse(_settings.GetOne("HydrationGoal")!.Value);
+        set => _settings.Update(new Setting
+        {
+            Key = "HydrationGoal",
+            Value = value.ToString(CultureInfo.CurrentCulture)
+        });
+    }
+
+    public decimal CalorieGoal
+    {
+        get => decimal.Parse(_settings.GetOne("CalorieGoal")!.Value);
+        set => _settings.Update(new Setting
+        {
+            Key = "CalorieGoal",
+            Value = value.ToString(CultureInfo.CurrentCulture)
+        });
+    }
 
     public void AddEntry(Entry entry) => _entries.Add(entry);
 
