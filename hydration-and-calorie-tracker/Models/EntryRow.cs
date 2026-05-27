58f1a86e79d3eff7dc127a3581622e98aa71d159
plskyuwu <gottsteinmatej@gmail.com>
2026-05-27 16:52:24 +0200

    docs(models): update summary

--- hydration-and-calorie-tracker/Models/EntryRow.cs
+++ hydration-and-calorie-tracker/Models/EntryRow.cs
@@ -1,7 +1,7 @@
 namespace hydration_and_calorie_tracker.Models;
 
 /// <summary>
-/// A simple wrapper record that combines and entry and the entry's item name
+/// A simple wrapper record that couples entry and the entry's item name into one object
 /// </summary>
 /// <param name="Entry">entry</param>
 /// <param name="ItemName">entry's item name</param>
