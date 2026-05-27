290ab964caa75f5bc85f278ef66aedf739666210
plskyuwu <gottsteinmatej@gmail.com>
2026-05-27 23:41:50 +0200

    feat(database): fix and change the deletion item system

--- hydration-and-calorie-tracker/Models/Database/TrackingService.cs
+++ hydration-and-calorie-tracker/Models/Database/TrackingService.cs
@@ -60,15 +60,62 @@ public class TrackingService(
 
     public bool DeleteEntry(int id) => entries.Delete(id);
 
-    public bool DeleteItem(int id) => items.Delete(id);
+    /// <summary>
+    /// Deletes an item by its id and entries using this item if <c>deleteEntries == true</c>
+    /// </summary>
+    /// <param name="id">item id</param>
+    /// <param name="deleteEntries">specifies if it also should delete all entries with this item's <c>id</c>, default value is <c>true</c></param>
+    /// <returns>returns true if an item was deleted and the count of removed entries</returns>
+    public (bool deleted, int entries) DeleteItem(int id,
+        bool deleteEntries = true)
+    {
+        var deleted = items.Delete(id);
+        var count = 0;
+
+        if (deleted && deleteEntries) count = DeleteAllBrokenEntries(id);
+
+        return (deleted, count);
+    }
 
     public int DeleteAllEntries() => entries.DeleteAll();
 
-    public int DeleteAllItems() => items.DeleteAll();
-
-    public int DeleteAllData()
+    public (int items, int entries) DeleteAllItems(bool deleteEntries = true)
     {
-        return DeleteAllEntries() + DeleteAllItems();
+        (int items, int entries) count = (0, 0);
+        count.items = items.DeleteAll();
+
+        if (count.items > 0 && deleteEntries)
+            count.entries = DeleteAllBrokenEntries();
+
+        return count;
+    }
+
+    public (int items, int entries) DeleteAllData()
+    {
+        var entriesCount = DeleteAllEntries();
+        var itemsCount = DeleteAllItems().items;
+        return (itemsCount, entriesCount);
+    }
+
+    public int DeleteAllBrokenEntries(int itemId)
+    {
+        var brokenEntryIds = GetAllEntries()
+            .Where(e => e.ItemId == itemId)
+            .Select(e => e.Id)
+            .ToList();
+
+        return brokenEntryIds.Count(entries.Delete);
+    }
+
+    public int DeleteAllBrokenEntries()
+    {
+        var validItemIds = GetAllItems().Select(i => i.Id).ToHashSet();
+        var brokenEntryIds = GetAllEntries()
+            .Where(e => !validItemIds.Contains(e.ItemId))
+            .Select(e => e.Id)
+            .ToList();
+
+        return brokenEntryIds.Count(entries.Delete);
     }
 
     public List<Entry> GetTodayEntries()
